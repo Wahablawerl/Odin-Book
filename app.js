d463b71852eb82e284226e450c8507246d510c94
Abdulwahab <abdulwahablawal2002@gmail.com>
2026-01-04 07:13:58 +0100

    implement auth for login, signin and logout

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require("express");
 const cors = require("cors");
 const dotenv = require("dotenv");
 const connectDB = require("./config/db");
+const authRoutes = require("./routes/authRoutes");
 
 // Load environment variables
 dotenv.config();
@@ -14,6 +15,7 @@ app.use(express.json());
 
 // Routes
 app.get("/", (req, res) => res.send("odinBook API Running..."));
+app.use("/api/auth", authRoutes);
 
 const PORT = process.env.PORT || 5000;
 
---
+++ controllers/authController.js
@@ -0,0 +1,61 @@
+const User = require("../models/userModel");
+const bcrypt = require("bcryptjs");
+const generateTokenAndSetCookie = require("../utils/generateToken");
+
+exports.signup = async (req, res) => {
+  try {
+    const { username, email, password } = req.body;
+
+    const userExists = await User.findOne({ $or: [{ email }, { username }] });
+    if (userExists)
+      return res.status(400).json({ message: "User already exists" });
+
+    // Hash Password
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(password, salt);
+
+    const newUser = await User.create({
+      username,
+      email,
+      password: hashedPassword,
+    });
+
+    if (newUser) {
+      generateTokenAndSetCookie(newUser._id, res);
+      res.status(201).json({
+        _id: newUser._id,
+        username: newUser.username,
+        email: newUser.email,
+      });
+    }
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+};
+
+exports.login = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+    const user = await User.findOne({ email });
+
+    // Compare plain text password with hashed password
+    const isPasswordCorrect = await bcrypt.compare(
+      password,
+      user?.password || ""
+    );
+
+    if (!user || !isPasswordCorrect) {
+      return res.status(400).json({ error: "Invalid email or password" });
+    }
+
+    generateTokenAndSetCookie(user._id, res);
+
+    res.status(200).json({
+      _id: user._id,
+      username: user.username,
+      profilePic: user.profilePic,
+    });
+  } catch (error) {
+    res.status(500).json({ error: "Server error during login" });
+  }
+};
